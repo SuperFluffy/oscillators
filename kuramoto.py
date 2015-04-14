9b5f6af662b34b6712a9447455842bd33f83f8bc
Richard Janis Beckert <janis.beckert@gmail.com>
2015-04-14 12:55:08 +0200

    Initial commit

---
+++ kuramoto.py
@@ -0,0 +1,84 @@
+# Array manipulation
+from numpy import append, asarray, empty, tile
+
+# Mathematical
+from numpy import abs, angle, imag, real
+from numpy import cos, exp, mean, mod, pi, sin, sum
+from numpy.random import normal, uniform
+
+class Kuramoto:
+    def __init__(self, size, coupling, stepsize, phases=None, nat_frequencies=None, stepcount=0):
+        self.size = size
+        self.coupling = coupling # uniform coupling
+
+        if phases is None:
+            self.phases = uniform(0, 2*pi, size)
+        else:
+            self.phases = asarray(phases)
+
+        if self.phases.size != size:
+            raise ValueError("Not enough initial phases for every oscillator in the network.")
+
+        if nat_frequencies is None:
+            self.nat_frequencies = normal(0 , 1, size)
+        else:
+            self.nat_frequencies = asarray(nat_frequencies)
+
+        if asarray(nat_frequencies).size == size:
+            raise ValueError("Not enough natural frequencies for every oscillator in the network.")
+
+        self.stepsize = stepsize
+        self.stepcount = stepcount
+
+    def make_step(self, update=True):
+# First order explicit forward Euler for now
+        tiled = tile(self.phases, (self.size, 1))
+        k1 = self.coupling * sum(sin(self.phases - tiled.T), axis=1)
+        if update:
+            self.phases += self.stepsize * (self.nat_frequencies + k1)
+            self.phases = mod(self.phases, 2*pi)
+            self.stepcount += 1
+        else:
+            return k1
+
+    def make_step_loop(self, update=True):
+# First order explicit forward Euler using explicit, slow loops
+        k1 = empty(self.size)
+        for i in range(0, self.size):
+            k1[i] = self.coupling * sum(sin(self.phases - self.phases[i]))
+
+        if update:
+            self.phases += self.stepsize * (self.nat_frequencies + k1)
+            self.phases = mod(self.phases, 2*pi)
+            self.stepcount += 1
+        else:
+            return k1
+
+    def order_parameters(self):
+        o = mean(exp(self.phases * complex(0,1)))
+        return abs(o), angle(o)
+
+    def rectangular(self):
+        p = exp(self.phases * complex(0,1))
+        return real(p), imag(p)
+
+def run(model, steps):
+    order_parameters = empty((2,steps))
+    for i in range(steps):
+        model.make_step()
+        order_parameters[:,i] = model.order_parameters()
+    return order_parameters
+
+
+steps = 1000
+
+N = 200
+#J = 0.5
+J = 4.0
+dt = 0.05
+model = Kuramoto(N, J/N, dt)
+
+order_parameters = run(model, steps)
+pyplot.plot(range(steps), order_parameters[0,:])
+#pyplot.plot(range(steps), order_parameters[1,:])
+#pyplot.show()
